3a8fd19b92101960a9f5e613ff9f026c8fa296f2
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2025-02-25 13:53:56 +0100

    ajout de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import twitchApi from "./services/twitchService";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Parcourir from "./pages/Parcourir";
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <BrowserRouter>
       <Navbar />
         <Routes>
+          <Route path="/HomePage" element={<HomePage />} />
+          <Route path="/parcourir" element={<Parcourir />} />
         </Routes>
       </BrowserRouter>
       
