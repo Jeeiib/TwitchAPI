6427c0cac6e5b051f84c88fcc2aefb0948f0802f
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2025-02-25 12:01:25 +0100

    ajout getTopGames and getTopStreamers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import twitchApi from './services/twitchService'
+import { useState } from "react";
+import "./App.css";
+import twitchApi from "./services/twitchService";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
-
-  return <>
-     
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+        </Routes>
+      </BrowserRouter>
     </>
-  
+  );
 }
 
-export default App
+export default App;
--- src/services/twitchService.js
+++ src/services/twitchService.js
@@ -4,39 +4,48 @@ const CLIENT_ID = "w7jrk3kepcklxgzvfpjbrkzajy7o4w";
 const CLIENT_SECRET = "2dkfcn3im8ztu8559kijhu6qt3ld4i";
 
 const twitchApi = axios.create({
-    baseURL: 'https://api.twitch.tv/helix/',
-    headers: {
-      'Client-ID': CLIENT_ID,
-    },
-  });
+  baseURL: "https://api.twitch.tv/helix/",
+  headers: {
+    "Client-ID": CLIENT_ID,
+  },
+});
 
 const getAccessToken = async () => {
-    try {
-        const response = await axios.post(
-            'https://id.twitch.tv/oauth2/token',
-            {
-              client_id: CLIENT_ID,
-              client_secret: CLIENT_SECRET,
-              grant_type: 'client_credentials',
-            },
-            {
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-              },
-            }
-          );
-          return response.data.access_token;
-    } catch (error) {
-        console.error('Erreur lors de la récupération du token :', error);
-        throw error;
-        
-    }
-}
+  try {
+    const response = await axios.post(
+      "https://id.twitch.tv/oauth2/token",
+      {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: "client_credentials",
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    return response.data.access_token;
+  } catch (error) {
+    console.error("Erreur lors de la récupération du token :", error);
+    throw error;
+  }
+};
 
 twitchApi.interceptors.request.use(async (config) => {
-    const token = await getAccessToken();
-    config.headers['Authorization'] = `Bearer ${token}`;
-    return config;
-    });
+  const token = await getAccessToken();
+  config.headers["Authorization"] = `Bearer ${token}`;
+  return config;
+});
+
+export const getTopGames = async (limit = 10) => {
+  try {
+    const response = await twitchApi.get(`games/top?first=${limit}`);
+    return response.data.data;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des jeux :", error);
+    throw error;
+  }
+};
 
 export default twitchApi;
