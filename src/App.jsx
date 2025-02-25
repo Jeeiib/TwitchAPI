bd011865e34d659b7b8fff64b18d7ce223f60520
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2025-02-25 12:36:15 +0100

    navbar route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import twitchApi from "./services/twitchService";
 import { BrowserRouter, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <Routes>
         </Routes>
       </BrowserRouter>
