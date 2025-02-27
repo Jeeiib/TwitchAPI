3436e9ec32fa29040eec6549dcfcebbd7fc1ac66
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2025-02-27 10:38:50 +0100

    supr api tags

---
+++ src/services/gameCategories.js
@@ -0,0 +1,72 @@
+const predefinedGameCategories = {
+    // Top Jeux de 2025
+    21779: ["Battle Royale", "Tir", "Action"],                 // Fortnite
+    32982: ["MOBA", "Stratégie", "Compétitif"],                // League of Legends
+    516575: ["FPS", "Compétitif", "Tactique"],                 // VALORANT
+    511224: ["FPS", "Battle Royale", "Action"],                // Apex Legends
+    27471: ["Sandbox", "Aventure", "Survie"],                  // Minecraft
+    32399: ["FPS", "Compétitif", "Action"],                    // Counter-Strike 2
+    509658: ["FPS", "Action", "Battle Royale"],                // Call of Duty: Modern Warfare/Warzone
+    512710: ["FPS", "Battle Royale", "Action"],                // Call of Duty: Warzone
+    33214: ["Sport", "Football", "Simulation"],                // EA Sports FC 24
+    138585: ["FPS", "Héros", "Action"],                        // Overwatch 2
+    513143: ["Action", "RPG", "Souls-like"],                   // Elden Ring
+    498566: ["Gacha", "RPG", "Open World"],                    // Genshin Impact
+    518379: ["FPS", "Battle Royale", "Action"],                // The Finals
+    491931: ["Action", "Roguelite", "2D"],                     // Vampire Survivors
+    498480: ["Action", "Open World", "Roleplay"],              // Grand Theft Auto V
+    491487: ["Horreur", "Multijoueur", "Survie"],              // Phasmophobia
+    29595: ["MMORPG", "Fantaisie", "JDR"],                     // World of Warcraft
+    509663: ["Aventure", "Open World", "Magie"],               // Hogwarts Legacy
+    493057: ["MMORPG", "JDR", "Aventure"],                     // Lost Ark
+    24241: ["MMORPG", "JDR", "Fantaisie"],                     // Final Fantasy XIV Online
+    491115: ["MOBA", "Stratégie", "Compétitif"],               // Dota 2
+    518380: ["Survie", "Crafting", "Pokémon-like"],            // Palworld
+    490377: ["Action", "Plateforme", "Aventure"],              // Super Mario Bros. Wonder
+    518273: ["Action", "Roguelike", "Metroidvania"],           // Lethal Company
+    509478: ["Combat", "Action", "Gore"],                      // Mortal Kombat 1
+    
+    // Jeux IRL et communautaires
+    509658: ["IRL", "Discussion", "Communauté"],               // Just Chatting
+    417752: ["IRL", "Créatif", "Art"],                         // Art
+    509660: ["IRL", "Compétition", "Sport"],                   // Sports
+    515214: ["Musique", "Performance", "Live"],                // Music
+    417790: ["IRL", "Relaxation", "Audio"],                    // ASMR
+    
+    // Jeux de stratégie et jeux de cartes
+    491572: ["Stratégie", "Cartes", "Deckbuilding"],           // Slay the Spire
+    29307: ["Cartes", "Stratégie", "Blizzard"],                // Hearthstone
+    512259: ["Jeu de cartes", "Stratégie", "Marvel"],          // Marvel Snap
+    491487: ["Survie", "Horreur", "Multijoueur"],              // Dead by Daylight
+    
+    // RPG et Aventure
+    505725: ["Action", "RPG", "Hack'n'slash"],                 // Diablo IV
+    1371363326: ["RPG", "D&D", "Stratégie"],                   // Baldur's Gate 3
+    502091: ["Action", "Aventure", "Open World"],              // Horizon Forbidden West
+    518222: ["JRPG", "Tour par tour", "Social links"],  
+  
+      // Jeux de combat et d'action
+      488615: ["Combat", "Anime", "Compétitif"],                 // Tekken 8
+      510578: ["Combat", "Arts martiaux", "Compétitif"],         // Street Fighter 6
+      518784: ["Combat", "Arts martiaux", "Action"],             // Sifu
+      
+      // Jeux de tir et shooters
+      491208: ["Tactical", "Survie", "Simulation"],              // Escape from Tarkov
+      518832: ["FPS", "Action", "Multijoueur"],                  // XDefiant
+      509670: ["FPS", "Science-fiction", "MMO"],                 // Destiny 2
+      506274: ["Battle Royale", "Survie", "Tir"],                // PUBG: BATTLEGROUNDS
+      
+      // Jeux d'horreur
+      518312: ["Horreur", "Survie", "Psychologique"],            // The Outlast Trials
+      512319: ["Horreur", "Survie", "Action"],                   // Resident Evil 4 Remake
+      515472: ["Horreur", "Thriller", "Survie"],                 // Alan Wake 2
+      
+      // Simulation et détente
+      490292: ["Roguelike", "Action", "Indie"],                  // The Binding of Isaac: Rebirth
+      488190: ["Stratégie", "Bac à sable", "Colonie"],           // RimWorld
+      
+      // Valeur par défaut
+      default: ["Jeux vidéo", "Divertissement", "Streaming"]
+    };
+
+export default predefinedGameCategories;
